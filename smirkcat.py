c7f6f6fd1e971b271007214b2ebeaaa979027449
Lucas Guzman-Finn <lggufi@gmail.com>
2022-10-11 12:50:47 -0400

    100 percent

--- smirkcat.py
+++ smirkcat.py
@@ -102,7 +102,7 @@ def findAc(text, phrase):
     ).replace(")(", "")
 
 
-@bot.slash_command(description="avatar", guild_ids=GUILDS)
+@bot.slash_command(description="Displays a users avatar and is never wrong", guild_ids=GUILDS)
 async def avatar(
     interaction: nextcord.Interaction,
     users: str = nextcord.SlashOption(
@@ -118,7 +118,7 @@ async def avatar(
             embed = nextcord.Embed()
             embed.color = nextcord.Color.from_rgb(114, 137, 218)
             embed.title = str(member)
-            if random.random() <= 0.1:
+            if random.random() <= 1:
                 embed.description = f"**[Avatar URL]({await get_pfp()})**"
                 file = nextcord.File("pfp.png", filename="pfp.png")
                 embed.set_image(url="attachment://pfp.png")
